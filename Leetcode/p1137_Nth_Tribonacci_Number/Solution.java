765e56df6d0995bfef6de73168baed3d8fed853a
Harshada Patil <harshadapatil5988@gmail.com>
2026-08-05 11:43:21 +0530

    feat: added nth tribonacci num\ problem

---
+++ Leetcode/p1137_Nth_Tribonacci_Number/Solution.java
@@ -0,0 +1,21 @@
+package com.p1137_Nth_Tribonacci_Number;
+
+public class Solution {
+
+	public int tribonacci(int n) {
+        if(n <= 1)
+            return n;
+        if(n == 2)
+            return 1;
+        int prev3 = 0, prev2 = 1, prev1 = 1;
+        int curr = 0;
+        for(int i=3; i<=n; i++){
+            curr = prev3 + prev2 + prev1;
+            prev3 = prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return curr;
+    }
+	 
+}
